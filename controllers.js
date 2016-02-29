012680e814bc00dea59f99b87fdf22b7c1335b0a
Marcelo Vargas <marcelo_vargas@gap.com>
2016-02-28 23:12:26 -0300

    usage of data binding instead of dom manipulation.

--- controllers.js
+++ controllers.js
@@ -1,8 +1,18 @@
 var angular = require('angular');
 var todolistApp = angular.module('todolistApp', []);
 
-todolistApp.controller('TodoListCtrl', function($scope){
-  $scope.addItem = function(){
-    console.log("oi");
+todolistApp.controller('TodoListCtrl', function(){
+
+  var todoList = this;
+
+  todoList.todos = [
+      {text:'learn angular', done:false},
+      {text:'learn webpack', done:false}
+  ];
+
+  todoList.addTodo = function(){
+    todoList.todos.push({text:todoList.todoText, done:false});
+    todoList.todoText = '';
   };
+
 });
