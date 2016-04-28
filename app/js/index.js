53fc44fcdb4966e33a59e98d1c1c715196df1d58
Marcelo Vargas <marcelo.vargas90@gmail.com>
2016-04-28 17:00:16 -0300

    extracting routing configuration into a specific file

--- app/js/index.js
+++ app/js/index.js
@@ -1,27 +1,8 @@
 var $ = require('jquery');
 window.$ = $;
 window.jQuery = $;
-require('bootstrap');
 require('bootstrap/dist/css/bootstrap.min.css');
 var angular = require('angular');
 require('angular-route');
-
-var todolistApp = angular.module('todolistApp', ['todolistControllers', 'ngRoute']);
-
-todolistApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/test', {
-        template: require('raw!../partials/test.html'),
-        controller: 'IndexController'
-      }).
-      when('/todolist', {
-        template: require('raw!../partials/todolist.html'),
-        controllerAs: 'TodoListCtrl'
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-  }]);
-
+require('./routes.js');
 require('./controllers/todolist-ctrl.js');
