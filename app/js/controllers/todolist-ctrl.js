0ce8448a303be23ab57464b2e2b6220c99459cc4
Marcelo Vargas <marcelo.vargas90@gmail.com>
2016-04-28 16:46:17 -0300

    start using scopes

---
+++ app/js/controllers/todolist-ctrl.js
@@ -0,0 +1,21 @@
+var todolistControllers = angular.module('todolistControllers', []);
+
+todolistControllers.controller('TodoListCtrl', ['$scope', function($scope){
+
+  $scope.todos = [
+      {text:'learn angular', done:false},
+      {text:'learn webpack', done:false}
+  ];
+
+  $scope.addTodo = function(){
+    $scope.todos.push({text:$scope.todoText, done:false});
+    $scope.todoText = '';
+  };
+
+}]);
+
+todolistControllers.controller('IndexController', ['$scope', function($scope){
+
+  $scope.mensagem = 'ola';
+
+}]);
